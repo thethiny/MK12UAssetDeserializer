4bc4b4c98937714f4d5517c028d7b72db7a04837
thethiny <thethiny@ymail.com>
2024-11-21 22:38:37 +0400

    Fix folder to use `auto` instead of `all`

--- get_all_files.ps1
+++ get_all_files.ps1
@@ -1,4 +1,4 @@
-$outPath = "automate_all"
+$outPath = "automate_auto"
 mkdir $outPath 2> $NULL 1> $NULL
 
 if (-not $args[0])
@@ -7,9 +7,19 @@ if (-not $args[0])
     exit
 }
 
+if ($args[1]) # if you pass a 2nd arg then it's inventory
+{
+    Write-Output "Inventories Only"
+    $likePath = "*\Inventory\*.uasset"
+} else
+{
+    $likePath = "*\*.uasset"
+}
+
 $Path = $args[0]
 
-foreach ($item in Get-ChildItem -Path $Path -Recurse -Filter *.uasset | Where-Object { $_.FullName -like "*\Inventory\*.uasset" })
+foreach ($item in Get-ChildItem -Path $Path -Recurse -Filter *.uasset | Where-Object { $_.FullName -like $likePath })
 {
     Copy-Item $item.fullname $outPath
+    Write-Output $item.fullname
 }
